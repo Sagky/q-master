ad4581b2bdef5dcd4669b59cade7e464285fac08
Sagky <ybwfjchtm8@privaterelay.appleid.com>
2026-07-01 00:05:13 +0200

    Fix landing page routing

--- q-master/server.js
+++ q-master/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { index: false })); 
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "landing.html"));
